5c908059246f4481cf89d84b13a0b0ca3dab0c86
Al Tobey <tobert@gmail.com>
2015-01-03 01:04:47 -0800

    update with ref to Caleb's reflex

--- src/tldr/my-blog-development-script.sh
+++ src/tldr/my-blog-development-script.sh
@@ -4,6 +4,7 @@
 # src/ directory and runs build-blog.go each time a file changes.
 #
 # pacman -S inotify-tools # for inotifywait on Arch Linux
+# TODO: use https://github.com/cespare/reflex instead
 
 while true
 do
